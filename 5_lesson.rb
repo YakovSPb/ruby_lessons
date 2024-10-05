2c391bd6b63e936b3dab4b0578ed6b198d725698
yakov <itmeetm@gmail.com>
2024-10-05 12:41:23 +0300

    find breeds

--- 5_lesson.rb
+++ 5_lesson.rb
@@ -1,9 +1,21 @@
 require 'faraday'
+require 'json'
 
-url = 'https://thecatapi.com/v1/breeds'
+puts 'Введите название породы собаки на английском'
+query = gets.strip.downcase
+
+url = 'https://api.thecatapi.com/v1/breeds/search?q=' + query
 
 token = '060465b0-40db-48a1-a93e-66c0c3471716'
 
 response = Faraday.get(url, {"Authorization" => "Bearer #{token}"})
 
-puts response.body
+raw_breeeds = JSON.parse(response.body)
+
+breeeds = raw_breeeds.map{|breed| breed['name']}
+
+puts breeeds.inspect
+
+
+# result 's'
+# ["Abyssinian", "American Shorthair", "Australian Mist", "Balinese", "British Longhair", "British Shorthair", "Burmese", "California Spangled", "Chausie", "Colorpoint Shorthair", "Cornish Rex", "Cyprus", "Exotic Shorthair", "Japanese Bobtail", "Javanese", "Norwegian Forest Cat", "Persian", "Russian Blue", "Savannah", "Scottish Fold", "Selkirk Rex", "Siamese", "Siberian", "Singapura", "Snowshoe", "Somali", "Sphynx", "Tonkinese", "Turkish Angora", "Turkish Van", "Donskoy"]
